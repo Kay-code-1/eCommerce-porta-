a9d833391e3f8f059562544a0e9be50a05c12231
kalyani joshi <kalyani121@gmail.com>
2021-09-15 22:12:53 -0400

    updated Product category relationship

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,7 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.hasOne(Category,{
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
 
